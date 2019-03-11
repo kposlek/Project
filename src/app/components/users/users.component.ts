22c43ee152c0e89ae48f676da0730d42b6e50050
Karolina Poslek <karolina@poslek.com>
2019-03-11 16:05:04 +0100

    Component & Template Clean Up

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -11,8 +11,6 @@ export class UsersComponent implements OnInit {
   showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
-  currentClasses = {};
-  currentStyles = {};
 
   constructor() {}
 
@@ -27,9 +25,9 @@ export class UsersComponent implements OnInit {
           city: 'Munich',
           state: 'Germany'
         },
-        image: 'http://lorempixel.com/600/600/people/3',
+
         isActive: true,
-        balance: 100,
+
         registered: new Date('01/02/2018 08:30:00')
       },
       {
@@ -41,9 +39,9 @@ export class UsersComponent implements OnInit {
           city: 'Munich',
           state: 'Germany'
         },
-        image: 'http://lorempixel.com/600/600/people/2',
+
         isActive: false,
-        balance: 200,
+
         registered: new Date('03/11/2017 06:20:00')
       },
       {
@@ -55,34 +53,15 @@ export class UsersComponent implements OnInit {
           city: 'Zagreb',
           state: 'Croatia'
         },
-        image: 'http://lorempixel.com/600/600/people/1',
+
         isActive: true,
-        balance: 50,
+
         registered: new Date('11/02/2016 10:30:00')
       }
     ];
     this.loaded = true;
-
-    // this.addUser({
-    //   firstName: 'Ivana',
-    //   lastName: 'Malenica'
-    // });
-    this.setCurrentClasses();
-    this.setCurrentStyles();
   }
   addUser(user: User) {
     this.users.push(user);
-  }
-  setCurrentClasses() {
-    this.currentClasses = {
-      'btn-success': this.enableAdd,
-      'big-text': this.showExtended
-    };
-  }
-  setCurrentStyles() {
-    this.currentStyles = {
-      'padding-top': this.showExtended ? '0' : '40px',
-      'font-size': this.showExtended ? '' : '40px'
-    };
   }
 }
