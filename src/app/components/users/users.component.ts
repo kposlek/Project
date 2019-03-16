b7129534b7bde6e9d54e68dc4411e1da84d1ba8f
Karolina Poslek <karolina@poslek.com>
2019-03-16 15:24:45 +0100

    ngModel & 2 WayData Binding

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/User';
+import { nullSafeIsEquivalent } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-users',
@@ -7,10 +8,20 @@ import { User } from '../../models/User';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user: User = {
+    firstName: '',
+    lastName: '',
+    age: null,
+    address: {
+      street: '',
+      city: '',
+      state: ''
+    }
+  };
   users: User[];
   showExtended: boolean = true;
   loaded: boolean = false;
-  enableAdd: boolean = true;
+  enableAdd: boolean = false;
   showUserForm: boolean = false;
 
   constructor() {}
@@ -62,8 +73,20 @@ export class UsersComponent implements OnInit {
     ];
     this.loaded = true;
   }
-  addUser(user: User) {
-    this.users.push(user);
+  addUser() {
+    this.user.isActive = true;
+    this.user.registered = new Date();
+    this.users.unshift(this.user);
+    this.user = {
+      firstName: '',
+      lastName: '',
+      age: null,
+      address: {
+        street: '',
+        city: '',
+        state: ''
+      }
+    };
   }
   //   fireEvent(e) {
   //     // console.log('Button Clicked');
